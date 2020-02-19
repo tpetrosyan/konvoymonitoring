563076fa17cb4c23174db669dfd1aac3ca116713
tpetrosyan <tpetrosyan.c@d2iq.com>
2020-02-19 02:24:11 +0000

    first push addons

---
+++ kubernetes-base-addons/README.md
@@ -0,0 +1,67 @@
+# Kubernetes Base Addons
+
+This repository contains the default sets of addons needed to bootstrap [D2iQ Kubernetes](https://d2iq.com/solutions/ksphere).
+
+# Overview
+
+The structure of this repository follows the [Kubeaddons Catalog Documentation](https://github.com/mesosphere/kubeaddons/blob/master/tools/catalog/README.md) in reference, and uses the [Addon Revision concept covered therein](https://github.com/mesosphere/kubeaddons/blob/master/tools/catalog/README.md#special-addonrepository-options---addon-revisions).
+
+You will find the following directories here:
+
+* `addons/` - containing the actual manifests for addon resources
+* `deployments/` - containing the default addons depending on the Kubernetes version
+* `metadata/` - containing static metadata for the addons in `addons/`
+* `test/` - containing integration tests for the addons in `addons/`
+
+# Support
+
+There is **no community support for this software at this time**, and this repository (while publicly visible) is only intended for consumption by D2iQ personnel.
+
+If you are here looking for addon support as an end-user, support is given as per the support provided for your [D2iQ Ksphere Solution](https://d2iq.com/solutions/ksphere)'s provided [enterprise support](https://d2iq.com/services-and-support) (solutions such as [Konvoy](https://d2iq.com/solutions/ksphere/konvoy)), but **support is not given *directly* via this repository**.
+
+Please [contact D2iQ](https://d2iq.com/contact) for questions and more information.
+
+# Releases
+
+Releases are tags which are cut from `master` and other branches and are intended to signify a single supported instance of `kubernetes-base-addons`.
+
+While you may see other tags in our [releases page](https://github.com/mesosphere/kubernetes-base-addons/releases) the only releases which are official and supported releases are those designated as non-prerelease and specifically mentioned to be official releases.
+
+**WARNING**: Do not use `master` for production, instead pick a supported release version.
+
+## Creating a Release
+
+Creating a releases is as simple as cutting a tag and making a [Github release](https://help.github.com/en/github/administering-a-repository/creating-releases).
+
+If the release is meant to be an official KSphere supported release, ensure it's not marked as a pre-release, follows the `stable-{MAJOR}.{MINOR}.{PATCH}-{REVISION}` pattern where the major, minor and patch version are that of the corresponding Kubernetes version. Ensure the language in the title and description indicate "Official Release".
+
+Some supported releases are supported via the terms of support for some other KSphere entity using them, particularly releases will be connected with a [Konvoy Release](https://github.com/mesosphere/konvoy). Make sure that you mention and link to any externally related sources for your release in the release description (e.g. if the release is specifically intended to support a specific Konvoy release, say so in the description).
+
+For all other non-official releases, make sure your tag and description are distinctly different from the official release pattern, explain the purpose of your release, and mark is as a `pre-release`.
+
+# Contributing
+
+Contributions to this repository are restricted to D2iQ personnel.
+
+See the [Kubeaddons Contributing Documentation](https://github.com/mesosphere/kubeaddons/blob/master/CONTRIBUTING.md) which provides the baseline information about how to contribute to this repository.
+
+Additionally see the sections below for notes about other rules and considerations for contributions.
+
+## Deprecation
+
+Sometimes you may want to **deprecate an older version (or revision) of an Addon**, for instance perhaps you have several revisions of the `v1.x` release of your addon, but now `v2.x` is out and you no longer wish to maintain the older major version.
+
+[Releases](/README.md#Releases) for this repository are responsible for snapshotting collections of revisions and will historically keep your deprecated and unsupported versions, so when you are ready to deprecate any major/minor version of your addon follow these steps:
+
+1. Create a PR that removes all files that are no longer supported
+2. Once the PR merges, a new minor release of this repository should be created indicating removal of support for the older versions
+
+Once this is complete end users who are using older releases of `kubernetes-base-addons` will be unaffected.
+
+## Addon Revisions
+
+You will find that any particular addon directory (e.g. `addons/prometheus`) may have several directories and several manifests nested in them each with variants of that addon. These are what we refered to above as "revisions".
+
+The **intention of revisions is to maintain a flat history of addon changes**. If you are making changes to any particular addon you should be making a revision of that addon as a copy of the original file with the changes made therein and the `addon-revision` version updated to reflect the new version appropriately.
+
+Right now adding revisions is a manual process (see [DCOS-62943](https://jira.mesosphere.com/browse/DCOS-62943) related to automating this in future iterations). To make it easier for reviewers to review PRs where revisions are [manually] added make the first commit in your branch a commit to ONLY copy the previous revision to the new revision file. That way the following commits can actually include your changes and will be easier to historically follow without needing to manually diff the files.
---
+++ kubernetes-base-addons/test/addons_test.go
@@ -0,0 +1,370 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/blang/semver"
+	volumetypes "github.com/docker/docker/api/types/volume"
+	docker "github.com/docker/docker/client"
+	"github.com/google/uuid"
+	"gopkg.in/yaml.v2"
+	"sigs.k8s.io/kind/pkg/apis/config/v1alpha3"
+	"sigs.k8s.io/kind/pkg/cluster"
+
+	"github.com/mesosphere/kubeaddons/hack/temp"
+	"github.com/mesosphere/kubeaddons/pkg/api/v1beta1"
+	"github.com/mesosphere/kubeaddons/pkg/repositories/local"
+	"github.com/mesosphere/kubeaddons/pkg/test"
+	"github.com/mesosphere/kubeaddons/pkg/test/cluster/kind"
+)
+
+const (
+	defaultKubernetesVersion = "1.15.6"
+	patchStorageClass        = `{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}`
+)
+
+var addonTestingGroups = make(map[string][]string)
+
+func init() {
+	b, err := ioutil.ReadFile("groups.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := yaml.Unmarshal(b, addonTestingGroups); err != nil {
+		panic(err)
+	}
+}
+
+func TestValidateUnhandledAddons(t *testing.T) {
+	unhandled, err := findUnhandled()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(unhandled) != 0 {
+		names := make([]string, len(unhandled))
+		for _, addon := range unhandled {
+			names = append(names, addon.GetName())
+		}
+		t.Fatal(fmt.Errorf("the following addons are not handled as part of a testing group: %+v", names))
+	}
+}
+
+func TestGeneralGroup(t *testing.T) {
+	if err := testgroup(t, "general"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestElasticsearchGroup(t *testing.T) {
+	if err := testgroup(t, "elasticsearch"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPrometheusGroup(t *testing.T) {
+	if err := testgroup(t, "prometheus"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestKommanderGroup(t *testing.T) {
+	if err := testgroup(t, "kommander"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestIstioGroup(t *testing.T) {
+	if err := testgroup(t, "istio"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestLocalVolumeProvisionerGroup(t *testing.T) {
+	if err := testgroup(t, "localvolumeprovisioner"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDispatchGroup(t *testing.T) {
+	if err := testgroup(t, "dispatch"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// -----------------------------------------------------------------------------
+// Private Functions
+// -----------------------------------------------------------------------------
+
+func createNodeVolumes(numberVolumes int, nodePrefix string, node *v1alpha3.Node) error {
+	dockerClient, err := docker.NewClientWithOpts(docker.FromEnv)
+	if err != nil {
+		return fmt.Errorf("creating docker client: %w", err)
+	}
+	dockerClient.NegotiateAPIVersion(context.TODO())
+
+	for index := 0; index < numberVolumes; index++ {
+		volumeName := fmt.Sprintf("%s-%d", nodePrefix, index)
+
+		volume, err := dockerClient.VolumeCreate(context.TODO(), volumetypes.VolumeCreateBody{
+			Driver: "local",
+			Name:   volumeName,
+		})
+		if err != nil {
+			return fmt.Errorf("creating volume for node: %w", err)
+		}
+
+		node.ExtraMounts = append(node.ExtraMounts, v1alpha3.Mount{
+			ContainerPath: fmt.Sprintf("/mnt/disks/%s", volumeName),
+			HostPath:      volume.Mountpoint,
+		})
+	}
+
+	return nil
+}
+
+func cleanupNodeVolumes(numberVolumes int, nodePrefix string, node *v1alpha3.Node) error {
+	dockerClient, err := docker.NewClientWithOpts(docker.FromEnv)
+	if err != nil {
+		return fmt.Errorf("creating docker client: %w", err)
+	}
+	dockerClient.NegotiateAPIVersion(context.TODO())
+
+	for index := 0; index < numberVolumes; index++ {
+		volumeName := fmt.Sprintf("%s-%d", nodePrefix, index)
+
+		if err := dockerClient.VolumeRemove(context.TODO(), volumeName, false); err != nil {
+			return fmt.Errorf("removing volume for node: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func testgroup(t *testing.T, groupname string) error {
+	t.Logf("testing group %s", groupname)
+
+	version, err := semver.Parse(defaultKubernetesVersion)
+	if err != nil {
+		return err
+	}
+
+	u := uuid.New()
+
+	node := v1alpha3.Node{}
+	if err := createNodeVolumes(3, u.String(), &node); err != nil {
+		return err
+	}
+	defer func() {
+		if err := cleanupNodeVolumes(3, u.String(), &node); err != nil {
+			t.Logf("error: %s", err)
+		}
+	}()
+
+	cluster, err := kind.NewCluster(version, cluster.CreateWithV1Alpha3Config(&v1alpha3.Cluster{
+		Nodes: []v1alpha3.Node{node},
+	}))
+	if err != nil {
+		return err
+	}
+	defer cluster.Cleanup()
+
+	if err := temp.DeployController(cluster, "kind"); err != nil {
+		return err
+	}
+
+	addons, err := addons(addonTestingGroups[groupname]...)
+	if err != nil {
+		return err
+	}
+
+	if err := removeLocalPathAsDefaultStorage(cluster, addons); err != nil {
+		return err
+	}
+
+	ph, err := test.NewBasicTestHarness(t, cluster, addons...)
+	if err != nil {
+		return err
+	}
+	defer ph.Cleanup()
+
+	ph.Validate()
+	ph.Deploy()
+
+	return nil
+}
+
+func addons(names ...string) ([]v1beta1.AddonInterface, error) {
+	var testAddons []v1beta1.AddonInterface
+
+	repo, err := local.NewRepository("base", "../addons")
+	if err != nil {
+		return testAddons, err
+	}
+	addons, err := repo.ListAddons()
+	if err != nil {
+		return testAddons, err
+	}
+
+	for _, addon := range addons {
+		for _, name := range names {
+			overrides(addon[0])
+			if addon[0].GetName() == name {
+				testAddons = append(testAddons, addon[0])
+			}
+		}
+	}
+
+	if len(testAddons) != len(names) {
+		return testAddons, fmt.Errorf("got %d addons, expected %d", len(testAddons), len(names))
+	}
+
+	return testAddons, nil
+}
+
+func findUnhandled() ([]v1beta1.AddonInterface, error) {
+	var unhandled []v1beta1.AddonInterface
+	repo, err := local.NewRepository("base", "../addons")
+	if err != nil {
+		return unhandled, err
+	}
+	addons, err := repo.ListAddons()
+	if err != nil {
+		return unhandled, err
+	}
+
+	for _, revisions := range addons {
+		addon := revisions[0]
+		found := false
+		for _, v := range addonTestingGroups {
+			for _, name := range v {
+				if name == addon.GetName() {
+					found = true
+				}
+			}
+		}
+		if !found {
+			unhandled = append(unhandled, addon)
+		}
+	}
+
+	return unhandled, nil
+}
+
+func removeLocalPathAsDefaultStorage(cluster test.Cluster, addons []v1beta1.AddonInterface) error {
+	for _, addon := range addons {
+		if addon.GetName() == "localvolumeprovisioner" {
+			if err := kubectl("patch", "storageclass", "local-path", "-p", patchStorageClass); err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	return nil
+}
+
+func kubectl(args ...string) error {
+	cmd := exec.Command("kubectl", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// -----------------------------------------------------------------------------
+// Private - CI Values Overrides
+// -----------------------------------------------------------------------------
+
+// TODO: a temporary place to put configuration overrides for addons
+// See: https://jira.mesosphere.com/browse/DCOS-62137
+func overrides(addon v1beta1.AddonInterface) {
+	if v, ok := addonOverrides[addon.GetName()]; ok {
+		addon.GetAddonSpec().ChartReference.Values = &v
+	}
+}
+
+var addonOverrides = map[string]string{
+	"dispatch": `
+---
+argo-cd:
+  prometheus:
+    enabled: true
+    release: prometheus-kubeaddons
+
+prometheus:
+  enabled: true
+  release: prometheus-kubeaddons
+
+minio:
+  persistence:
+    size: 1Gi
+`,
+	"metallb": `
+---
+configInline:
+  address-pools:
+  - name: default
+    protocol: layer2
+    addresses:
+    - "172.17.1.200-172.17.1.250"
+`,
+	"istio": `
+---
+      kiali:
+       enabled: true
+       contextPath: /ops/portal/kiali
+       ingress:
+         enabled: true
+         kubernetes.io/ingress.class: traefik
+         hosts:
+           - ""
+       dashboard:
+         auth:
+           strategy: anonymous
+       prometheusAddr: http://prometheus-kubeaddons-prom-prometheus.kubeaddons:9090
+
+      tracing:
+        enabled: true
+        contextPath: /ops/portal/jaeger
+        ingress:
+          enabled: true
+          kubernetes.io/ingress.class: traefik
+          hosts:
+            - ""
+
+      grafana:
+        enabled: true
+
+      prometheus:
+        serviceName: prometheus-kubeaddons-prom-prometheus.kubeaddons
+
+      istiocoredns:
+        enabled: true
+
+      security:
+       selfSigned: true
+       caCert: /etc/cacerts/tls.crt
+       caKey: /etc/cacerts/tls.key
+       rootCert: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+       certChain: /etc/cacerts/tls.crt
+       enableNamespacesByDefault: false
+
+      global:
+       podDNSSearchNamespaces:
+       - global
+       - "{{ valueOrDefault .DeploymentMeta.Namespace \"default\" }}.global"
+
+       mtls:
+        enabled: true
+
+       multiCluster:
+        enabled: true
+
+       controlPlaneSecurityEnabled: true
+`,
+}
